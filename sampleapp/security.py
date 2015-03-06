f226964b0945d1eb4f51e6a3501b72496abf7e1f
FEI FAN <feifan.pub@gmail.com>
2015-03-05 17:56:24 -0800

    remove redundant  code

--- sampleapp/security.py
+++ sampleapp/security.py
@@ -2,14 +2,10 @@ import json
 
 
 def my_mongocall_perm_check(req, db, col, cmd):
-    # u = req.user
-
-    if db != "dbforapp1":  # limit db to dbforapp1
+    if db != "db1":  # limit db to db1
         return False
 
-    if not col.startswith("transaction_"):  # limit collection to transaction_.*
+    if not col.startswith("user"):  # limit collection to transaction_.*
         return False
 
-    q = json.loads(req.GET.get("criteria", "{}"))  # limit parameter based on criteria
-    # todo limit q
     return True

b453889b20fbd826df01ff56c372b8bec4048289
dio <contact@dionysio.com>
2017-08-19 01:50:40 +0200

    changed deprecated url patterns

--- djmongoreader/urls.py
+++ djmongoreader/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from . import views
 
-urlpatterns = patterns('',
-                       url(r"^$", views.blank, name="dj-mongo-reader-root"),
-                       url(r"^info/$", views.info),
-                       url(r"^(?P<db>\w+)/(?P<col>\w+)/$", views.blank, name="dj-mongo-reader-col"),
-                       url(r"^(?P<db>\w+)/(?P<col>\w+)/_(?P<cmd>\w+)$", views.restcall, name="dj-mongo-reader-cmd"),
-                       url(r"^(?P<db>\w+)/(?P<col>\w+)/exportcsv/$", views.exportcsv, {"cmd":"exportcsv"}, name="dj-mongo-reader-exportcsv"),
-                       )
+urlpatterns = [
+   url(r"^$", views.blank, name="dj-mongo-reader-root"),
+   url(r"^info/$", views.info),
+   url(r"^(?P<db>\w+)/(?P<col>\w+)/$", views.blank, name="dj-mongo-reader-col"),
+   url(r"^(?P<db>\w+)/(?P<col>\w+)/_(?P<cmd>\w+)$", views.restcall, name="dj-mongo-reader-cmd"),
+   url(r"^(?P<db>\w+)/(?P<col>\w+)/exportcsv/$", views.exportcsv, {"cmd":"exportcsv"}, name="dj-mongo-reader-exportcsv"),
+]

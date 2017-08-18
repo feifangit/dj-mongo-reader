aaec77953aa978334e7811f3f93bcd453709af68
dio <contact@dionysio.com>
2017-08-19 01:51:04 +0200

    changed deprecated url patterns in sampleapp

--- example/sampleapp/sampleapp/urls.py
+++ example/sampleapp/sampleapp/urls.py
@@ -1,11 +1,20 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'sampleapp.views.readme', name='home'),
-    url(r'^query/$', 'sampleapp.views.query',),
+
+urlpatterns = [
+    url(r'^$', views.readme, name='home'),
+    url(r'^query/$', views.query,),
     url(r'^mongo/', include('djmongoreader.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

0551565752b5a47bafb2282868cd1ac42fc749d2
cfchen <cfchen@futuredial.com>
2018-09-18 16:17:03 +0800

    Add replica set support.

--- djmongoreader/models.py
+++ djmongoreader/models.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from pymongo import MongoClient, MongoReplicaSetClient
+from pymongo.read_preferences import ReadPreference
 from .mongoreader import MongoHandler
 
 _MONGO_CONN_STR = settings.MONGO_READER_SETTINGS.get("conn_str")
 
 _conn = MongoClient(_MONGO_CONN_STR)
 try:  # to support pymongo 2.x
-	if _conn._MongoClient__repl:
-	    _conn = MongoReplicaSetClient(_MONGO_CONN_STR)
+	if settings.MONGO_READER_SETTINGS.get("replicaset"):
+	    _conn = MongoReplicaSetClient(_MONGO_CONN_STR, replicaSet=settings.MONGO_READER_SETTINGS.get("replicaset"), ReadPreference = ReadPreference.PRIMARY_PREFERRED)
 except:
 	pass
 
